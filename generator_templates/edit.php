814a76b022512161f8a67e8a352af0b15a5139b4
Jordan Lev <github@jordanlev.com>
2011-06-03 12:23:59 -0700

    cleaned up some code generation -- do not output things that are not needed

--- generator_templates/edit.php
+++ generator_templates/edit.php
@@ -1,8 +1,5 @@
 <?php 
 defined('C5_EXECUTE') or die("Access Denied.");
-$al = Loader::helper('concrete/asset_library'); //for image and file fields
-$ps = Loader::helper('form/page_selector', 'designer_content'); //for image links and page link fields
-$dt = Loader::helper('form/date_time'); //for date picker fields
-?>
+[[[GENERATOR_REPLACE_HELPERLOADERS]]]?>
 
 [[[GENERATOR_REPLACE_FIELDS]]]
--- generator_templates/view.php
+++ generator_templates/view.php
@@ -1,6 +1,5 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
-$nh = Loader::helper('navigation'); //for page links
-?>
+[[[GENERATOR_REPLACE_HELPERLOADERS]]]?>
 
 [[[GENERATOR_REPLACE_HTML]]]
